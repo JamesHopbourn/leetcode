4fddfaa6077ef8d4b95aadf3734417dcffc56829
胡金栋 <jameshopbourn@gmail.com>
2023-07-27 15:30:34 +0800

    add reference url
    Signed-off-by: 胡金栋 <jameshopbourn@gmail.com>

--- src/W2D2StringWithKMP/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/W2D2StringWithKMP/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -6,6 +6,7 @@ import common.PrintUtil;
  * https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string
  * https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/solution/shua-chuan-lc-shuang-bai-po-su-jie-fa-km-tb86/
  * https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/solution/dai-ma-sui-xiang-lu-kmpsuan-fa-xiang-jie-mfbs/
+ * https://programmercarl.com/0028.%E5%AE%9E%E7%8E%B0strStr.html#%E5%85%B6%E4%BB%96%E8%AF%AD%E8%A8%80%E7%89%88%E6%9C%AC
  */
 public class FindTheIndexOfTheFirstOccurrenceInAString {
     public static void main(String[] args) {
