71b72735b226b509725a6dac3fcf4f3141a82878
胡金栋 <jameshopbourn@gmail.com>
2023-07-27 08:40:58 +0800

    review RansomNote
    Signed-off-by: 胡金栋 <jameshopbourn@gmail.com>

--- src/W1D6Hash/RansomNote.java
+++ src/W1D6Hash/RansomNote.java
@@ -38,6 +38,9 @@ public class RansomNote {
     }
 
     public static boolean function(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()){
+            return false;
+        }
         int[] alphabet = new int[26];
         for (int i = 0; i < magazine.length(); i++) {
             alphabet[magazine.charAt(i) - 'a']++;
