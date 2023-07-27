06b2f75b50c140efe38563dde1505d1c86cfee68
胡金栋 <jameshopbourn@gmail.com>
2023-07-27 17:39:21 +0800

    ImplementStackUsingQueues
    Signed-off-by: 胡金栋 <jameshopbourn@gmail.com>

---
+++ src/W2D3QueueStacks/ImplementQueueUsingStacks.java
@@ -0,0 +1,62 @@
+package W2D3QueueStacks;
+
+import java.util.Stack;
+
+/**
+ * https://ls8sck0zrg.feishu.cn/wiki/C7kpwXv20iHShwkeBTycevGPnGe <p>
+ *
+ * https://leetcode.cn/problems/implement-queue-using-stacks/
+ */
+public class ImplementQueueUsingStacks {
+    public static void main(String[] args) {
+        MyQueue obj = new MyQueue();
+        obj.push(1);
+        obj.push(2);
+        obj.push(3);
+        int param_2 = obj.pop();
+        System.out.println("param_2 = " + param_2);
+        int param_3 = obj.peek();
+        System.out.println("param_3 = " + param_3);
+        boolean queueIsEmpty = obj.empty();
+        System.out.println("param_4 = " + queueIsEmpty);
+    }
+
+    static class MyQueue {
+        private final Stack<Integer> inStack;
+        private final Stack<Integer> outStack;
+
+        public MyQueue() {
+            inStack = new Stack<>();
+            outStack = new Stack<>();
+        }
+
+        public void push(int x) {
+            inStack.push(x);
+        }
+
+        public int pop(){
+            inStackMoveOutStack();
+            Integer pop = outStack.pop();
+            return pop;
+        }
+
+        private void inStackMoveOutStack() {
+            if (outStack.isEmpty()){
+                while (!inStack.isEmpty()){
+                     outStack.push(inStack.pop());
+                }
+            }
+        }
+
+        public int peek() {
+            inStackMoveOutStack();
+            Integer pop = outStack.pop();
+            outStack.push(pop);
+            return pop;
+        }
+
+        public boolean empty() {
+            return inStack.isEmpty() && outStack.isEmpty();
+        }
+    }
+}
---
+++ src/W2D3QueueStacks/ImplementStackUsingQueues.java
@@ -0,0 +1,54 @@
+package W2D3QueueStacks;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode.cn/problems/implement-stack-using-queues/ <p>
+ * https://www.bilibili.com/video/BV1nY4y1w7VC/ <p>
+ *
+ * https://www.liaoxuefeng.com/wiki/1252599548343744/1265121791832960 <p>
+ * https://leetcode.cn/problems/implement-stack-using-queues/solution/yong-dui-lie-shi-xian-zhan-by-leetcode-solution/ <p>
+ */
+public class ImplementStackUsingQueues {
+    public static void main(String[] args) {
+        MyStack obj = new MyStack();
+        obj.push(1);
+        obj.push(2);
+        obj.push(3);
+        int param_2 = obj.pop();
+        System.out.println("param_2 = " + param_2);
+        int param_3 = obj.top();
+        System.out.println("param_3 = " + param_3);
+        boolean param_4 = obj.empty();
+        System.out.println("param_4 = " + param_4);
+    }
+
+    static class MyStack {
+        Queue<Integer> queue;
+
+        public MyStack() {
+            queue = new LinkedList<Integer>();
+        }
+
+        public void push(int x) {
+            int size = queue.size();
+            queue.offer(x);
+            for(int i=0;i<size;i++){
+                queue.offer(queue.poll());
+            }
+        }
+
+        public int pop() {
+            return queue.poll();
+        }
+
+        public int top() {
+            return queue.peek();
+        }
+
+        public boolean empty() {
+            return queue.isEmpty();
+        }
+    }
+}
