ccc1a00522efa10128ae9f727ac46df52012cb28
胡金栋 <jameshopbourn@gmail.com>
2023-07-30 06:54:56 +0800

    最大岛屿面积
    Signed-off-by: 胡金栋 <jameshopbourn@gmail.com>

---
+++ src/Islands/MaxAreaOfIsland.java
@@ -0,0 +1,44 @@
+package Islands;
+
+public class MaxAreaOfIsland {
+    public static void main(String[] args) {
+        System.out.println(maxAreaOfIsland(new int[][]{{1}}));
+        System.out.println(maxAreaOfIsland(new int[][]{
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        }));
+    }
+
+    public static int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                // 不要忘了这边也要做个判断 == 1
+                if (grid[i][j] == 1){
+                    max = Math.max(max, dfs(grid, i, j));
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        // 计算面积总和
+        int area = 1;
+        area += dfs(grid, i + 1, j);
+        area += dfs(grid, i - 1, j);
+        area += dfs(grid, i, j - 1);
+        area += dfs(grid, i, j + 1);
+        return area;
+    }
+}
--- src/Islands/NumberOfIslands.java
+++ src/Islands/NumberOfIslands.java
@@ -32,17 +32,17 @@ public class NumberOfIslands {
         return islandNum;
     }
 
-    public static void dfs(int[][] grid, int r, int c) {
-        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length) {
+    public static void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
             return;
         }
-        if (grid[r][c] == 0) {
+        if (grid[i][j] == 0) {
             return;
         }
-        grid[r][c] = 0;
-        dfs(grid, r - 1, c);
-        dfs(grid, r + 1, c);
-        dfs(grid, r, c + 1);
-        dfs(grid, r, c - 1);
+        grid[i][j] = 0;
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 }
