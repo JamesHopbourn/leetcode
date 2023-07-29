0f0d56a88a843b6d7030664f60b2ce67eb806f45
胡金栋 <jameshopbourn@gmail.com>
2023-07-29 23:17:10 +0800

    mergeTree 代码简化版本
    Signed-off-by: 胡金栋 <jameshopbourn@gmail.com>

--- src/W3D5Tree/MergeTwoBinaryTreesBFS.java
+++ src/W3D5Tree/MergeTwoBinaryTreesBFS.java
@@ -68,6 +68,13 @@ public class MergeTwoBinaryTreesBFS {
         }
         return merged;
     }
+
+    /**
+     * 下面这个是代码简化版本
+     * @param t1
+     * @param t2
+     * @return
+     */
     public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
         if (t1 == null){
             return t2;
@@ -82,38 +89,35 @@ public class MergeTwoBinaryTreesBFS {
         mergeQueue.add(merged);
         leftQueue.add(t1);
         rightQueue.add(t2);
+        // 两个 BFS 叠加 逐步去写就基本没问题了
         while (!leftQueue.isEmpty() && !rightQueue.isEmpty()){
             TreeNode node = mergeQueue.poll();
             TreeNode node1 = leftQueue.poll();
             TreeNode node2 = rightQueue.poll();
-            TreeNode left1 = node1.left;
-            TreeNode left2 = node2.left;
-            TreeNode right1 = node1.right;
-            TreeNode right2 = node2.right;
-            if (left1 != null || left2 != null){
-                if (left1 != null && left2 != null){
-                    TreeNode left = new TreeNode(left1.val + left2.val);
+            if (node1.left != null || node2.left != null){
+                if (node1.left != null && node2.left != null){
+                    TreeNode left = new TreeNode(node1.left.val + node2.left.val);
                     node.left = left;
                     mergeQueue.add(left);
-                    leftQueue.add(left1);
-                    rightQueue.add(right2);
-                } else if (left1 != null){
-                    node.left = left1;
-                } else if (left2 != null){
-                    node.left = left2;
+                    leftQueue.add(node1.left);
+                    rightQueue.add(node2.left);
+                } else if (node1.left != null){
+                    node.left = node1.left;
+                } else if (node2.left != null){
+                    node.left = node2.left;
                 }
             }
-            if (right1 != null || right2 != null){
-                if (right1 != null && right2 != null){
-                    TreeNode right = new TreeNode(right1.val + right2.val);
+            if (node1.right != null || node2.right != null){
+                if (node1.right != null && node2.right != null){
+                    TreeNode right = new TreeNode(node1.right.val + node2.right.val);
                     node.right = right;
                     mergeQueue.add(right);
-                    leftQueue.add(right1);
-                    rightQueue.add(right2);
-                } else if (right1 != null){
-                    node.right = right1;
-                } else if (right2 != null){
-                    node.right = right2;
+                    leftQueue.add(node1.right);
+                    rightQueue.add(node2.right);
+                } else if (node1.right != null){
+                    node.right = node1.right;
+                } else if (node2.right != null){
+                    node.right = node2.right;
                 }
             }
         }
