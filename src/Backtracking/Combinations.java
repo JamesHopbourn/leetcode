0ef2afe1f381e82dcf69c83725efe92ab146cbc4
胡金栋 <jameshopbourn@gmail.com>
2023-07-30 20:25:10 +0800

    回溯基础入门
    Signed-off-by: 胡金栋 <jameshopbourn@gmail.com>

--- src/Backtracking/Combinations.java
+++ src/Backtracking/Combinations.java
@@ -1,4 +1,36 @@
 package Backtracking;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Combinations {
+    public static void main(String[] args) {
+        System.out.println(combine(4, 2));
+    }
+
+    static List<List<Integer>> result = new ArrayList<>();
+    static LinkedList<Integer> path = new LinkedList<>();
+
+    public static List<List<Integer>> combine(int n, int k) {
+        backtracking(n, k, 1);
+        return result;
+    }
+
+    public static void backtracking(int n, int k, int startIndex) {
+        // 终止条件
+        if (path.size() == k) {
+            // 存放结果
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = startIndex; i <= n; i++) {
+            // 处理节点
+            path.add(i);
+            // 递归
+            backtracking(n, k, i + 1);
+            // 回溯
+            path.removeLast();
+        }
+    }
 }
--- src/Backtracking/Permutations.java
+++ src/Backtracking/Permutations.java
@@ -1,47 +1,40 @@
 package Backtracking;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Demo {
+/**
+ * <a href="https://leetcode.cn/problems/permutations/submissions/">46. 全排列 - 力扣（LeetCode）</a>
+ * <a href="https://leetcode.cn/problems/permutations/solution/hui-su-suan-fa-python-dai-ma-java-dai-ma-by-liweiw/">回溯算法入门级详解 + 练习（持续更新） - 全排列 - 力扣（LeetCode）</a>
+ * <a href="https://labuladong.github.io/algo/di-san-zha-24031/bao-li-sou-96f79/hui-su-sua-c26da/">回溯算法解题套路框架 | labuladong 的算法小抄</a>
+ */
+public class Permutations {
     public static void main(String[] args) {
-        System.out.println(permute(new int[]{1, 2, 3, 4}));
+        Permutations permutations = new Permutations();
+        System.out.println(permutations.permute(new int[]{1, 2, 3, 4}));
     }
-    
-    static List<List<Integer>> res = new LinkedList<>();
 
-    /* 主函数，输入一组不重复的数字，返回它们的全排列 */
-    static List<List<Integer>> permute(int[] nums) {
-        // 记录「路径」
-        LinkedList<Integer> track = new LinkedList<>();
-        // 「路径」中的元素会被标记为 true，避免重复使用
-        backtrack(nums, track);
+    List<List<Integer>> res = new ArrayList<>();
+
+    List<List<Integer>> permute(int[] nums) {
+        LinkedList<Integer> result =  new LinkedList<>();
+        backtracking(nums, result);
         return res;
     }
 
-    // 路径：记录在 track 中
-    // 选择列表：nums 中不存在于 track 的那些元素（used[i] 为 false）
-    // 结束条件：nums 中的元素全都在 track 中出现
-    static void backtrack(int[] nums, LinkedList<Integer> track) {
-        // 触发结束条件
-        if (track.size() == nums.length) {
-            res.add(new LinkedList(track));
+    void backtracking(int[] nums, LinkedList<Integer> result){
+        if (result.size() == nums.length){
+            res.add(new LinkedList<>(result));
             return;
         }
-
         for (int i = 0; i < nums.length; i++) {
-            // 排除不合法的选择
-            if (track.contains(nums[i])) {
-                // nums[i] 已经在 track 中，跳过
+            if (result.contains(nums[i])){
                 continue;
             }
-            // 做选择
-            track.add(nums[i]);
-            // 进入下一层决策树
-            backtrack(nums, track);
-            // 取消选择
-            track.removeLast();
-            
+            result.add(nums[i]);
+            backtracking(nums, result);
+            result.removeLast();
         }
     }
 }
