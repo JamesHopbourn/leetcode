8262a91ee00780befe4d060868e73896d762ec04
胡金栋 <jameshopbourn@gmail.com>
2023-07-30 13:38:19 +0800

    对称二叉树
    Signed-off-by: 胡金栋 <jameshopbourn@gmail.com>

--- src/W3D1Tree/SymmetricTreeBFS.java
+++ src/W3D1Tree/SymmetricTreeBFS.java
@@ -5,11 +5,19 @@ import common.TreeNode;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * <a href="https://leetcode.cn/problems/symmetric-tree/">101. 对称二叉树 - 力扣（LeetCode）</a>
+ * <a href="https://leetcode.cn/problems/symmetric-tree/solution/di-gui-101-dui-cheng-er-cha-shu-by-turin-4r7d/">【递归】101. 对称二叉树</a>
+ * <a href="https://leetcode.cn/problems/symmetric-tree/solution/dong-hua-yan-shi-101-dui-cheng-er-cha-shu-by-user7/">动画演示+多种实现 101. 对称二叉树</a>
+ */
 public class SymmetricTreeBFS {
     public static void main(String[] args) {
         System.out.println(isSymmetric(TreeNode.createTree(new Integer[]{1, 2, 2, 3, 4, 4, 3})));
     }
 
+    /**
+   下面的逻辑思路和 {@link SymmetricTreeDFS} 大同小异，只是表现形式不同
+   */
     public static boolean isSymmetric(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
--- src/W3D1Tree/SymmetricTreeDFS.java
+++ src/W3D1Tree/SymmetricTreeDFS.java
@@ -3,12 +3,14 @@ package W3D1Tree;
 import common.TreeNode;
 
 /**
+ * <a href="https://leetcode.cn/problems/symmetric-tree/">101. 对称二叉树 - 力扣（LeetCode）</a>
+ * <a href="https://leetcode.cn/problems/symmetric-tree/solution/di-gui-101-dui-cheng-er-cha-shu-by-turin-4r7d/">【递归】101. 对称二叉树</a>
  * <a href="https://leetcode.cn/problems/symmetric-tree/solution/dong-hua-yan-shi-101-dui-cheng-er-cha-shu-by-user7/">动画演示+多种实现 101. 对称二叉树</a>
- * 
  */
 public class SymmetricTreeDFS {
     public static void main(String[] args) {
         System.out.println(isSymmetric(TreeNode.createTree(new Integer[]{1, 2, 2, 3, 4, 4, 3})));
+        System.out.println(isSymmetric(TreeNode.createTree(new Integer[]{1, 2, 2, null, 3, null, 3})));
     }
 
     public static boolean isSymmetric(TreeNode root) {
