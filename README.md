0fa8eadf3afeb4d5538ec23f9bea58970ddb6ba1
胡金栋 <jameshopbourn@gmail.com>
2023-07-30 21:30:31 +0800

    全排列代码优化
    Signed-off-by: 胡金栋 <jameshopbourn@gmail.com>

--- README.md
+++ README.md
@@ -11,3 +11,9 @@
   - 求路径和：BFS 使用两个 Queue 处理
 - W3D5Tree
   - 合并两棵树：BFS 使用三个 Queue 处理
+
+#### 学习参考资料
+- [azl397985856/leetcode题解，记录自己的leetcode解题之路](https://github.com/azl397985856/leetcode)
+- [labuladong 的算法小抄](https://labuladong.github.io/algo/home/)
+- [代码随想录](https://programmercarl.com/)
+- [【labuladong】回溯算法核心套路详解_哔哩哔哩_bilibili](https://www.bilibili.com/video/BV1P5411N7Xc/)  
--- src/Backtracking/PermutationWithMemo.java
+++ src/Backtracking/PermutationWithMemo.java
@@ -9,33 +9,39 @@ import java.util.List;
  * <a href="https://leetcode.cn/problems/permutations/solution/hui-su-suan-fa-python-dai-ma-java-dai-ma-by-liweiw/">回溯算法入门级详解 + 练习（持续更新） - 全排列 - 力扣（LeetCode）</a> <p>
  * <a href="https://labuladong.github.io/algo/di-san-zha-24031/bao-li-sou-96f79/hui-su-sua-c26da/">回溯算法解题套路框架 | labuladong 的算法小抄</a> <p>
  */
-public class Permutations {
+public class PermutationWithMemo {
     public static void main(String[] args) {
-        new Permutations().permute(new int[]{1, 2, 3}).forEach(System.out::println);
+        new PermutationWithMemo().permute(new int[]{1, 2, 3}).forEach(System.out::println);
     }
 
     List<List<Integer>> result = new ArrayList<>();
 
     List<List<Integer>> permute(int[] nums) {
-        LinkedList<Integer> track = new LinkedList<>();
         // 使用 LinkedList<Integer> 而不是 List<Integer>
-        backtracking(nums, track);
+        LinkedList<Integer> track = new LinkedList<>();
+        boolean[] used = new boolean[nums.length];
+        backtracking(nums, track, used);
         return result;
     }
 
-    void backtracking(int[] nums, LinkedList<Integer> track) {
+    void backtracking(int[] nums, LinkedList<Integer> track, boolean[] used) {
         if (track.size() == nums.length) {
             // 注意这里要 new LinkedList<>(track) 而不能直接 add(track)
             result.add(new LinkedList<>(track));
             return;
         }
-        for (int num : nums) {
-            if (track.contains(num)) {
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]){
                 continue;
             }
-            track.add(num);
-            backtracking(nums, track);
+            // 选中元素
+            used[i] = true;
+            track.add(nums[i]);
+            // 回溯
+            backtracking(nums, track, used);
+            // 回溯弹出
             track.removeLast();
+            used[i]= false;
         }
     }
 }
